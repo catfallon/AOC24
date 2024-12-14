a7f595e02bafe6f9bddc8b3b5c85ddcbc4783608
Cat Fallon <fallon.146@osu.edu>
2024-12-14 14:19:05 -0500

    Finished Day 11 Pt 1

--- Day11/Day11.py
+++ Day11/Day11.py
@@ -1,43 +1,41 @@
 #I am just completely skipping around let's go
-
-file = open(".\\Day11\\inputexample.txt")
-blinks = 3;
+import time
+start_time = time.time()
+file = open(".\\Day11\\input.txt")
+blinks = 25
 rocks = file.read().split()
-rocksCopy = rocks.copy()
+
 for x in range(blinks):
-    #then do stuff
     rockCount = 0
-    
-    for y in rocks: # wait this doesnt make sense because I have to do it again
-        print("start of inside loop")
-        print("rocksCount is " + str(rockCount))
-        print("rocks is " + str(rocks))
-        print("being turned into " + str(rocksCopy))
-        print()
+    rocksCopy = rocks.copy()
+    for y in rocks:
         if int(y) == 0:
-            rocksCopy[rockCount] = "1"
+            if rockCount >= len(rocksCopy):
+                rocksCopy.append("1")
+            else:
+                rocksCopy[rockCount] = "1"
         elif len(str(y)) % 2 == 0:
-            #this is pretty crazy actually
             firstString = y[:int(len(y) / 2)]
             secondString = y[int(len(y) / 2):]
-            #print(firstString)
-            #print(secondString)
-            print("before insertion, rocksCopy is " + str(rocksCopy))
             rocksCopy.insert(rockCount, firstString)
-            #print("str is " + str(int(secondString)))
-            
-            print(rocksCopy)
-            print("rockCount is " + str(rockCount))
-            rocksCopy[rockCount] = str(int(secondString))
             rockCount += 1
-            #not sure what to do about this actually
+            if rockCount >= len(rocksCopy):
+                rocksCopy.append(str(int(secondString)))
+            else:
+                rocksCopy[rockCount] = str(int(secondString))
+            
         else:
-            rocksCopy[rockCount] = str(int(y) * 2024)
+            if rockCount >= len(rocksCopy):
+                rocksCopy.append(str(int(y) * 2024))
+            else:
+                rocksCopy[rockCount] = str(int(y) * 2024)
+        
         rockCount += 1
-
     rocks = rocksCopy
-    print(rocks)
 
-    x += 1
 
-print(rocksCopy)
+#print(rocksCopy)
+#print(rocks)
+print(len(rocks))
+
+print("Process finished --- %s seconds ---" % (time.time() - start_time))
